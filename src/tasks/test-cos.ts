587d8f0d7f565dfa5bba430897903533fe603122
赵一一 <1940988349@qq.com>
2026-05-05 20:22:50 +0800

    feat: 增加jwt鉴权

--- src/tasks/test-cos.ts
+++ src/tasks/test-cos.ts
@@ -1,20 +1,32 @@
-import { sendEmail } from "../core";
-import config from "../config";
+import { SignJWT } from 'jose';
+
+import { sendEmail } from '../core';
+import config from '../config';
+
+function generateToken(data: Record<string, unknown>, secret: string) {
+  return new SignJWT(data)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime('4h')
+    .sign(new TextEncoder().encode(secret));
+}
 
 export async function main() {
-  const res = await fetch(
-    `https://workers.19981105.xyz/common/get-cos-auth?authKey=${config.COS_AUTH_KEY}`,
-  );
+  const res = await fetch(`https://workers.19981105.xyz/common/get-cos-auth?authKey=${config.COS_AUTH_KEY}`,  {
+    headers: {
+      'Authorization': `Bearer ${generateToken({}, config.COS_AUTH_KEY )}`
+    }
+  });
   const data = (await res.json()) as {
     data: unknown;
     code: number;
     msg: string;
   };
   if (data.code !== 0) {
-    await sendEmail("获取 COS 授权失败", JSON.stringify(data));
+    await sendEmail('获取 COS 授权失败', JSON.stringify(data));
     return;
   }
-  console.log("cos授权成功");
+  console.log('cos授权成功');
 }
 
 main();
